c36cdde163402787a75c2fd977e0008c72fd4074
Andreas Tille <tille@debian.org>
2018-06-19 14:19:03 +0200

    New upstream version 0.6.2

--- test/integration/load_node_test.cpp
+++ test/integration/load_node_test.cpp
@@ -198,5 +198,44 @@ TEST(NodeTest, ParseNodeStyle) {
   EXPECT_EQ(EmitterStyle::Block, Load("- foo\n- bar").Style());
   EXPECT_EQ(EmitterStyle::Block, Load("foo: bar").Style());
 }
+
+struct ParserExceptionTestCase {
+  std::string name;
+  std::string input;
+  std::string expected_exception;
+};
+
+TEST(NodeTest, IncompleteJson) {
+  std::vector<ParserExceptionTestCase> tests = {
+      {"JSON map without value", "{\"access\"", ErrorMsg::END_OF_MAP_FLOW},
+      {"JSON map with colon but no value", "{\"access\":",
+       ErrorMsg::END_OF_MAP_FLOW},
+      {"JSON map with unclosed value quote", "{\"access\":\"",
+       ErrorMsg::END_OF_MAP_FLOW},
+      {"JSON map without end brace", "{\"access\":\"abc\"",
+       ErrorMsg::END_OF_MAP_FLOW},
+  };
+  for (const ParserExceptionTestCase test : tests) {
+    try {
+      Load(test.input);
+      FAIL() << "Expected exception " << test.expected_exception << " for "
+             << test.name << ", input: " << test.input;
+    } catch (const ParserException& e) {
+      EXPECT_EQ(test.expected_exception, e.msg);
+    }
+  }
 }
+
+TEST(NodeTest, LoadTildeAsNull) {
+  Node node = Load("~");
+  ASSERT_TRUE(node.IsNull());
 }
+    
+TEST(NodeTest, LoadTagWithParenthesis) {
+    Node node = Load("!Complex(Tag) foo");
+    EXPECT_EQ(node.Tag(), "!Complex(Tag)");
+    EXPECT_EQ(node.as<std::string>(), "foo");
+}
+
+}  // namespace
+}  // namespace YAML
